641e920dbde21d43a3d9b94883a009b5abfbdc69
bhagyashri111 <kshirsagar.bhagyashri@gmail.com>
2023-12-12 17:21:19 -0500

    Committed

---
+++ src/com/perscholas/java_basics/IntToDouble.java
@@ -0,0 +1,21 @@
+package com.perscholas.java_basics;
+
+public class IntToDouble {
+
+    public static void main(String[] args){
+        Integer x=5;
+        Integer y=6;
+
+        Integer q=y/x;
+        System.out.println("Division of y/x is :"+ q);
+
+
+
+        Double d=Double.valueOf(y);
+
+        System.out.println(d);
+
+
+
+    }
+}
---
+++ src/com/perscholas/java_basics/JavaBasicsClass.java
@@ -0,0 +1,57 @@
+package com.perscholas.java_basics;
+
+public class JavaBasicsClass {
+
+    public static void main(String[] args){
+//sum of two integer variables
+        int a = 20;
+        int b= 10;
+        int c = a+b;
+        System.out.println("Sum of two Integer numbers: " + c);
+// sum of two double
+        double x = 10.5;
+        double y= 12.5;
+        double z = x+y;
+        System.out.println("Sum of two double numbers: " + z);
+// sum of Integer and double
+        Integer d = 10;
+        double e= 12.5;
+        double f= x+y;
+        System.out.println("Sum of Integer and double numbers: " + f);
+// sum of two integers and divide
+        Integer g = 40;
+        Integer h= 20;
+        Integer i = g/h;
+        System.out.println("Divide two numbers: " + i);
+// larger integer to decimal
+        double j = 40.0;
+        Integer k= 20;
+        double l = g/h;
+        System.out.println("Divide larger decimal and Integer: " + l);
+//sum of two double and cast
+        double m = 40.0;
+        double n= 20.0;
+        double o = g/h;
+        System.out.println("Divide double numbers: " + o);
+
+//Conversion of double into Integer
+
+        // Get the double value
+        double data = 3452.345;
+        System.out.println("Double - " + data);
+
+        // convert into int
+        int value = (int)data;
+
+        // print the int value
+        System.out.println("Integer - " + value);
+//
+
+
+
+
+
+    }
+
+    }
+
